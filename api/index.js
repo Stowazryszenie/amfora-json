f53df60606ac04d58b927fab58832dae199edaaa
Stowazryszenie <sce.amfora@gmail.com>
2025-07-03 15:20:30 +0200

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,15 +1,12 @@
+// api/index.js
+
 export default async function handler(req, res) {
-  try {
-    const response = await fetch("https://script.google.com/macros/s/AKfycbxO5YQ1-Lft_XSFWIR6f2CCxQUY-BvUGKGdAtT469LcUjIC2S1YlySBvi1R5f-XnWdChA/exec");
-
-    if (!response.ok) {
-      throw new Error(`HTTP error! Status: ${response.status}`);
-    }
-
-    const data = await response.json();
-    res.status(200).json(data);
-  } catch (error) {
-    console.error("Error:", error);
-    res.status(500).json({ error: "Internal Server Error" });
+  const response = await fetch('https://script.google.com/macros/s/AKfycbxO5YQ1-Lft_XSFWIR6f2CCxQUY-BvUGKGdAtT469LcUjIC2S1YlySBvi1R5f-XnWdChA/exec');
+  
+  if (!response.ok) {
+    return res.status(500).json({ error: 'Failed to fetch data from Google Apps Script' });
   }
+
+  const data = await response.json();
+  res.status(200).json(data);
 }
