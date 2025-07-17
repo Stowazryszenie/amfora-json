72e29e01fe2aba1cd53d4e6a31ad78e4bf625adf
Stowazryszenie <sce.amfora@gmail.com>
2025-07-17 15:03:26 +0200

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 // api/index.js
 
 export default async function handler(req, res) {
-  const response = await fetch('https://script.google.com/macros/s/AKfycbzyn7WEgOm03fbqV36XjgIx2fBTx9PnrkxDaDxLCWUgYW6OnziTc9yS6NrdwRb7GOAnyg/exec');
+  const response = await fetch('https://script.google.com/macros/s/AKfycbwbxj3Fnr-z6jFXhvGugjRGIdUGpit6tojvX7DJz5foXhdcNsihrkkXgH24Bs4uFW4NZA/exec');
   
   if (!response.ok) {
     return res.status(500).json({ error: 'Failed to fetch data from Google Apps Script' });
